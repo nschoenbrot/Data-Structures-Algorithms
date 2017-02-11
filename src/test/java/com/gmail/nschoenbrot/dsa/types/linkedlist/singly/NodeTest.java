8ce07a01c94221d715f1fe75a50cc8c593721b27
nschoenbrot <nschoenbrot@gmail.com>
2017-02-11 15:26:37 -0500

    Implemented a singly linked list.

---
+++ src/test/java/com/gmail/nschoenbrot/dsa/types/linkedlist/singly/NodeTest.java
@@ -0,0 +1,47 @@
+package com.gmail.nschoenbrot.dsa.types.linkedlist.singly;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Nelson Schoenbrot
+ */
+public class NodeTest {
+    private Node<Integer> node;
+
+    @Before
+    public void setUp() throws Exception {
+        node = new Node<>(1);
+    }
+
+    @Test
+    public void append() throws Exception {
+        final Node<Integer> a = new Node<>(2);
+        final Node<Integer> b = new Node<>(3);
+        final Node<Integer> c = new Node<>(4);
+        node.append(a, b, c);
+        assertEquals("1, 2, 3, 4", node.listAsString());
+    }
+
+    @Test
+    public void getNext() throws Exception {
+        final Node<Integer> a = new Node<>(2);
+        final Node<Integer> b = new Node<>(3);
+        final Node<Integer> c = new Node<>(4);
+        node.append(a, b, c);
+        assertEquals(a, node.getNext());
+        assertEquals(b, node.getNext().getNext());
+        assertEquals(c, node.getNext().getNext().getNext());
+    }
+
+    @Test
+    public void reverse() throws Exception {
+        final Node<Integer> a = new Node<>(2);
+        final Node<Integer> b = new Node<>(3);
+        final Node<Integer> c = new Node<>(4);
+        node.append(a, b, c);
+        assertEquals("4, 3, 2, 1", node.reverse().listAsString());
+    }
+}
